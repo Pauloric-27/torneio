0e6861f19a7102151257138f21774287a7ac7fcd
Paulo Ricardo <poulr27@gmail.com>
2023-12-20 13:44:07 -0300

    Atualizei o index criando uma tabela e atualizei o model produto

--- src/main/java/com/campeonato/torneio/models/Time.java
+++ src/main/java/com/campeonato/torneio/models/Time.java
@@ -4,11 +4,43 @@
  */
 package com.campeonato.torneio.models;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 /**
  *
  * @author 20211114010023
  */
 public class Time {
+    
+    private int id;
+    private String nome;
+    private String estado;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    public void salvar (JdbcTemplate jdbc){
+        jdbc.update("INSERT INTO times (nome, estado) " + "VALUES (?, ?)",  (ps) -> 
+        {ps.setString(1, nome);
+        ps.setString(2, estado);});
+    }
+    
     
     
 }
